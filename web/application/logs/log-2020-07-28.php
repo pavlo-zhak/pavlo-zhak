f3f94684f7e3ae25d01ee5a5b1809a10d60aa67f
Pavlo Zhak <p.zhak@magneticone.com>
2020-07-29 09:20:27 +0300

    Initial commit

---
+++ web/application/logs/log-2020-07-28.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-07-28 09:39:32 --> 404 Page Not Found: Faviconico/index
+ERROR - 2020-07-28 09:43:57 --> 404 Page Not Found: Faviconico/index
+ERROR - 2020-07-28 09:43:58 --> 404 Page Not Found: Faviconico/index
+ERROR - 2020-07-28 09:43:59 --> 404 Page Not Found: Faviconico/index
+ERROR - 2020-07-28 17:10:11 --> 404 Page Not Found: Faviconico/index
+ERROR - 2020-07-28 17:10:34 --> 404 Page Not Found: Faviconico/index
